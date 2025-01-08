d36c4e27ab0cbefbc4a8b2a0141eda223e960b14
Kyaw Thura <kanaiyaaryal@gmail.com>
2025-01-08 22:56:51 +0700

    reproduce

---
+++ app/index.tsx
@@ -0,0 +1,33 @@
+import { useLocalSearchParams, useRouter } from "expo-router";
+import { View, Text } from "react-native";
+import { useRoute as useRNRoute } from "@react-navigation/native";
+
+export default function HomeScreen() {
+  const router = useRouter();
+  const rnRoute = useRNRoute();
+  const params = useLocalSearchParams();
+  const sortBy = params.sortBy;
+  console.log({ sortBy, rnSortBy: rnRoute.params?.sortBy });
+
+  return (
+    <View style={{ flex: 1, marginTop: "30%" }}>
+      <Text>
+        Expo: {sortBy} || React Navigation:{rnRoute.params?.sortBy}
+      </Text>
+      <Text>
+        Type: {typeof sortBy}: {typeof rnRoute.params?.sortBy}
+      </Text>
+
+      <Text
+        style={{ marginTop: 10 }}
+        onPress={() =>
+          router.setParams({
+            sortBy: sortBy === "oldest" ? undefined : "oldest",
+          })
+        }
+      >
+        Sort By: Oldest
+      </Text>
+    </View>
+  );
+}
